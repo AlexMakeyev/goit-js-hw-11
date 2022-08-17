a9928f6d225f59ab49ed79e54e626996dc2ea5fd
Alex Makeyev <makeyev1987@gmail.com>
2022-08-17 23:34:30 +0300

    pre-build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,84 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
-import photoCardTpl from './partials/photoCard.hbs';
+// import photoCard from './partials/photoCard.hbs'
+import markUp from './js/photoTpl';
 import makeSearch from './js/search.js';
 import { config } from './js/config';
 import "notiflix/dist/notiflix-3.2.5.min.css";
 import getRefs from './js/getRefs';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = getRefs();
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+refs.loadMoreBtn.classList.add('disabled');
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+function onSearchFormSubmit(event) {
+    event.preventDefault();
+    if (!inputFieldValidation(event)) {
+        return;
+    } ;      
+    makeSearch( config).then(data => {        
+        if (data.totalHits === 0) {
+            config.search = '';
+            return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        };       
+        config.page += 1;
+        refs.loadMoreBtn.classList.remove('disabled');
+        config.totalHits += config.perPage;
+        event.target.reset();
+        makeMarkUp(data);             
+    })
+        .catch(error => errorHandling());    
+};
+function onLoadMoreBtnClick() {
+    
+    makeSearch( config).then(data => {
+        config.totalHits += config.perPage;
+        
+        makeMarkUp(data);
+        if (config.totalHits >= data.totalHits) {
+            refs.loadMoreBtn.classList.add('disabled');
+            config.search = '';
+            return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        };
+        
+    })
+        .catch(error => errorHandling());
+    config.page += 1;
+
+
+};
+
+function makeMarkUp(data) {
+    const searchElement = data.hits.map(markUp).join('');
+    refs.gallery.insertAdjacentHTML('beforeend', searchElement);  
+};
+function clearMarkUp() {
+    refs.gallery.innerHTML = ' ';
+};
+function inputFieldValidation(event) {
+    if (event.currentTarget.elements.searchQuery.value.trim() === '') {
+        clearMarkUp();
+        refs.loadMoreBtn.classList.add('disabled');
+        Notiflix.Notify.failure("Enter somethig, please");
+        return false;
+    };    
+    if (event.currentTarget.elements.searchQuery.value.trim() !== config.search) {
+                
+        config.search = event.currentTarget.elements.searchQuery.value;
+        config.page = 1;
+        refs.loadMoreBtn.classList.add('disabled');
+        config.totalHits = 0;
+        clearMarkUp();
+        return true;
+    };    
+};
+function errorHandling() {
+    clearMarkUp();
+            config.search = '';
+            refs.loadMoreBtn.classList.add('disabled');
+        return Notiflix.Notify.failure("OOps, something went wrong"); 
+};
+
---
+++ src/js/photoTpl.js
@@ -0,0 +1,22 @@
+// import photoCardTpl from '../partials/photoCard.hbs'
+export default function markUp({ webformatURL, tags, likes, views, comments, downloads }) {
+    
+   
+ return  `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class='gallery__image' />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+};
